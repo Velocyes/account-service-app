748d21a164f621d98bc13c40cac71b159b328a3d
Findryankp <findryankurnia@gmail.com>
2023-02-13 14:15:41 +0700

    fix history balanc

--- entities/historyBalance.go
+++ entities/historyBalance.go
@@ -6,6 +6,8 @@ type HistoryBalance struct {
 	gorm.Model
 	UserId        int          `json:"user_id"`
 	User          *User        `gorm:"foreignKey:UserId"`
+	UserIdTo      int          `json:"user_id_to"`
+	UserTo        *User        `gorm:"foreignKey:UserIdTo"`
 	BalanceTypeId int          `json:"balance_type_id"`
 	BalanceType   *BalanceType `gorm:"foreignKey:BalanceTypeId"`
 	Total         int          `json:"total"`
