a602387d1b52ed9acef752e1ab9d64a20c3ec7fc
Findryankp <findryankurnia@gmail.com>
2023-02-13 13:01:14 +0700

    add meu

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "account-service-app/menu"
+
+func main() {
+	menu.MenuAuth()
+}
---
+++ menu/menu.go
@@ -0,0 +1,20 @@
+package menu
+
+import "fmt"
+
+func MenuAuth() {
+	fmt.Println("1. Register")
+	fmt.Println("2. Login")
+}
+
+func MainMenu() {
+	fmt.Println("1. Read Account")
+	fmt.Println("2. Update Account")
+	fmt.Println("3. Delete Account")
+	fmt.Println("4. Top Up")
+	fmt.Println("5. Transfer")
+	fmt.Println("6. History Top Up")
+	fmt.Println("7. History Transfer")
+	fmt.Println("8. Profile Another User")
+	fmt.Println("0. Exit")
+}
