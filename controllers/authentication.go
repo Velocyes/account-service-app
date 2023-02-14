5e8091cf6bed8094bdb4146f7c9199ba1581d6d2
Findryankp <findryankurnia@gmail.com>
2023-02-14 10:10:21 +0700

    has password

--- controllers/authentication.go
+++ controllers/authentication.go
@@ -5,6 +5,8 @@ import (
 	"account-service-app/entities"
 	"fmt"
 	_ "log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var LoggedInUser entities.User
@@ -17,15 +19,22 @@ func Login() bool {
 	fmt.Scanln(&password)
 	fmt.Println()
 
-	querySelectUser := "SELECT u.id, u.name, u.phone_number FROM users u WHERE u.phone_number = ? AND password = ?"
-	error := config.DB.QueryRow(querySelectUser, phoneNumber, password).Scan(&user.ID, &user.Name, &user.PhoneNumber)
+	querySelectUser := "SELECT u.id, u.name, u.phone_number,u.password FROM users u WHERE u.phone_number = ?"
+	error := config.DB.QueryRow(querySelectUser, phoneNumber).Scan(&user.ID, &user.Name, &user.PhoneNumber, &user.Password)
 	if error != nil {
 		LoggedInUser = entities.User{}
 		fmt.Println("Credential is wrong or user doesn't exists")
 		fmt.Println()
 	} else {
-		LoggedInUser = user
-		return true
+		//check if password is true
+		if CheckPasswordHash(password, user.Password) {
+			LoggedInUser = user
+			return true
+		}
+
+		LoggedInUser = entities.User{}
+		fmt.Println("Credential is wrong or user doesn't exists")
+		fmt.Println()
 	}
 	return false
 }
@@ -43,6 +52,7 @@ func Register() bool {
 	queryCreateUser := "SELECT u.phone_number FROM users u WHERE u.phone_number = ?"
 	error := config.DB.QueryRow(queryCreateUser, user.PhoneNumber).Scan(&phoneNumber)
 	if error != nil {
+		user.Password, _ = HashPassword(user.Password)
 		result, errInsert := config.DB.Exec("INSERT INTO users (name, phone_number, password) VALUES (?, ?, ?)", user.Name, user.PhoneNumber, user.Password)
 		if errInsert != nil {
 			fmt.Println(errInsert.Error())
@@ -53,14 +63,14 @@ func Register() bool {
 			LoggedInUser = user
 		}
 
-		//Insert initial balance
+		//Insert Initial Balance
 		saldo := 0
 		fmt.Printf("Masukan saldo awal user : ")
 		fmt.Scanln(&saldo)
 		user_id, _ := result.LastInsertId()
 		config.DB.Exec("INSERT INTO balances (user_id, total_balance) VALUES (?, ?)", user_id, saldo)
 
-		fmt.Println("Register success")
+		fmt.Println("Register user success")
 		fmt.Println()
 		return true
 	} else {
@@ -72,4 +82,14 @@ func Register() bool {
 
 func Logout() {
 	LoggedInUser = entities.User{}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
